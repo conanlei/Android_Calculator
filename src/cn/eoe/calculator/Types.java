6d7b2a21569de13f2beada33bf366d9f24adcabf
conanlei <514376839@qq.com>
2016-07-23 09:27:07 +0800

    first commit

---
+++ src/cn/eoe/calculator/Types.java
@@ -0,0 +1,11 @@
+package cn.eoe.calculator;
+
+public class Types {
+
+	public static final int ADD = 1;
+	public static final int SUB = 2;
+	public static final int X = 3;
+	public static final int DIV = 4;
+	public static final int NUM = 5;
+	
+}
